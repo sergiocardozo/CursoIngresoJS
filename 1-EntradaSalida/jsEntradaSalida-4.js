d11e0b23f68ba1c938664e83d242263dd1020530
sergiocardozo <52476421+sergiocardozo@users.noreply.github.com>
2019-07-03 00:22:26 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,16 @@
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
+{ var name;
+	document.getElementById("elNombre").value = name;
+	document.getElementById("elNombre").value;
+	prompt("ingrese su nombre");
+
+	
+	
+	
+	
+	
 
 }
 
