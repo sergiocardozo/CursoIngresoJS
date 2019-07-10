2e16bdc5d64eba3e0bfe97706f4eadc092b3c064
sergiocardozo <52476421+sergiocardozo@users.noreply.github.com>
2019-07-09 21:50:57 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,21 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad = document.getElementById("edad").value;
+
+if (edad >= 18) {
+
+     alert("Es mayor");
+
+    }
+
+else if (edad < 18) {
+
+     alert("Es menor");
+
+    }
+
 
 }//FIN DE LA FUNCIÓN
