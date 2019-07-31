74986d1b9562646e5803c3c2bfdd8f328a97d794
sergiocardozo <52476421+sergiocardozo@users.noreply.github.com>
2019-07-30 22:17:47 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -3,6 +3,10 @@ function mostrar()
 
 var repetciones = prompt("ingrese el número de repeticiones");
 
+for ( var i = 1; i <= repetciones  ; i++ ) {
+
+    document.write ( " Hola UTN FRA " + ("<br>") );
+}
 
 
 }//FIN DE LA FUNCIÓN
